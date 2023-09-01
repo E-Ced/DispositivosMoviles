91e79db431bb2fef6c61426059d078a1b254f1fb
E-Ced <88070528+E-Ced@users.noreply.github.com>
2023-09-01 11:20:51 -0600

    Add files via upload

--- home_page.dart
+++ home_page.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
-bool c1 = false;
-bool c2 = false;
-bool c3 = false;
-bool c4 = false;
+
+class count extends StatefulWidget {
+  const count({super.key});
+
+  @override
+  State<count> createState() => _countState();
+}
+
+class _countState extends State<count> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+var contador = 0;
+bool click1 = false;
+bool click2 = false;
+bool click3 = false;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,64 +31,132 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("McFlutter"),
+        title: Text("Mi Iteso"),
       ),
-      body: Container(
-        margin: EdgeInsets.all(10),
-        height: 180,
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.black)
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                  padding: EdgeInsets.all(8),
-                  child: Icon(Icons.account_circle, size: 50,),
-                  ),
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
+      body: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+            ),
+            child: SizedBox(
+              height: 250,
+              width: MediaQuery.of(context).size.width - 5,
+            ),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text("Flutter McFlutter", style: Theme.of(context).textTheme.headlineSmall,),
-                      Text("Experienced App Developer"),
+                      Container(
+                        padding: const EdgeInsets.only(bottom: 8),
+                        child: const Text(
+                          'El ITESO, Universidad Jesuita de Gudalajara',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        'San Pedro Tlaquepaque, Jal.',
+                        style: TextStyle(
+                          color: Colors.grey[500],
+                        ),
+                      ),
                     ],
                   ),
+                ),
+                IconButton(onPressed: (){contador++; setState(() {});}, icon: Icon(Icons.thumb_up), color: Colors.blue),
+                SizedBox(width: 10,),
+                Text(
+                  contador.toString(),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        click1 = !click1;
+                        setState(() {});
+                        ScaffoldMessenger.of(context)
+                          ..hideCurrentSnackBar()
+                          ..showSnackBar(
+                            SnackBar(content: Text("No tienes ningún correo")),
+                          );
+                      },
+                      icon: Icon(Icons.email),
+                      color: (click1 == false ? Colors.black : Colors.indigo),
+                      iconSize: 50),
+                  Text("Correo"),
                 ],
               ),
-              SizedBox(
-                height: 8,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              Column(
                 children: [
-                  Text("123 Main Street"),
-                  Text("+52 33 2375 8501")
+                  IconButton(
+                      onPressed: () {
+                        click2 = !click2;
+                        setState(() {});
+                        ScaffoldMessenger.of(context)
+                          ..hideCurrentSnackBar()
+                          ..showSnackBar(
+                            SnackBar(content: Text("No tienes ninguna llamada")),
+                          );
+                      }, icon: Icon(Icons.phone), color: (click2 == false ? Colors.black : Colors.indigo), iconSize: 50),
+                  Text("Llamada"),
                 ],
               ),
-              SizedBox(
-                height: 16,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+              Column(
                 children: [
-                  IconButton(onPressed: (){c1 = !c1; setState((){});}, icon: Icon(Icons.accessibility), color: c1 == false ? Colors.black : Colors.indigo,),
-                  IconButton(onPressed: (){c2 = !c2; setState((){});}, icon: Icon(Icons.timer), color: c2 == false ? Colors.black : Colors.indigo,),
-                  IconButton(onPressed: (){c3 = !c3; setState((){});}, icon:  Icon(Icons.phone_android), color: c3 == false ? Colors.black : Colors.indigo,),
-                  IconButton(onPressed: (){c4 = !c4; setState((){});}, icon:   Icon(Icons.phone_iphone), color: c4 == false ? Colors.black : Colors.indigo,),
+                  IconButton(
+                      onPressed: () {click3 = !click3; setState((){});
+                      ScaffoldMessenger.of(context)
+                          ..hideCurrentSnackBar()
+                          ..showSnackBar(
+                            SnackBar(content: Text("La ruta se ha copiado al portapapeles")),
+                          );
+                      },
+                      icon: Icon(
+                        Icons.directions,
+                      ),
+                      color: click3 == false ? Colors.black : Colors.indigo,
+                      iconSize: 50),
+                  Text("Ruta"),
                 ],
               ),
             ],
           ),
-        ),
+          SizedBox(
+            height: 64,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width - 70,
+                child: Text(
+                  "El ITESO, Universidad Jesuita de Guadalajara (Instituto Tecnológico y de Estudios Superiores de Occidente) es una universidad privada ubicada en la Zona Metropolitana de Guadalajara, Jalisco, México, fundada en el año 1957. La institución forma parte del Sistema Universitario Jesuita (SUJ) que integra a ocho universidades en México. La universidad es nombrada como la Universidad Jesuita de Guadalajara. Historia. Fundada en el año de 1957 por el Padre Luis Hernández Prieto S.J.,José Aguilar Figueroa y el ingeniero José Fernández del Valle y Ancira, entre otros, la universidad ha tenido una larga trayectoria. A continuación se presenta la historia de la institución en periodos de décadas.",
+                  textAlign: TextAlign.justify,
+                ),
+              )
+            ],
+          ),
+        ],
       ),
-
     );
   }
 }
--- main.dart
+++ main.dart
@@ -1,16 +1,19 @@
-import 'home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:mi_iteso/home_page.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'McFlutter',
-      home: HomePage()
-    );
+    return MaterialApp(title: 'Mi ITESO', home: HomePage());
   }
 }
